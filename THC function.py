7cec793a232d510a6b8a99be10ff4d25b205fbeb
Hongsik-Kim <52567223+Hongsik-Kim@users.noreply.github.com>
2019-07-05 13:56:27 +0100

    Add files via upload

---
+++ THC function.py
@@ -0,0 +1,69 @@
+""" 
+Introduction to Machine Learning
+
+Course work 1
+
+@author : Hongsik. Kim
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import time
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+os.chdir('/Users/congcondk/Desktop/CASS/Term3/Machine Learning/Coursework/Output')
+
+#=========================================================================================================================================================================================================================
+
+def Three_Hump_Camel(x):
+    x1, x2 = x
+    return 2*(x1**2) - 1.05*(x1**4) + (x1**6)/6 + (x1*x2) + (x2**2)
+
+def Three_Hump_Camel_gradient(x):
+    x1,x2=x
+    f1 = 4*x1 - 4.2*(x1**3) + (x1**5) + x2
+    f2 = x1 + 2*x2
+    return np.array([f1,f2])
+
+#=========================================================================================================================================================================================================================
+
+
+x1 = np.linspace(-2,2,100)
+x2 = np.linspace(-2,2,100)
+x1grid, x2grid = np.meshgrid(x1, x2)
+xx = np.stack([x1grid, x2grid])
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x1grid, x2grid, Three_Hump_Camel(xx),cmap=cm.gist_rainbow)
+ax.set_title('Three-Hump Camel function')
+ax.set_xlabel('x1')
+ax.set_ylabel('x2')
+
+plt.show()
+
+
+#=========================================================================================================================================================================================================================
+
+from gd_2d import gd_2d_plain, gd_2d_momentum, gd_2d_NAG
+
+x0 = [2,2]
+# Plain vanilla gd
+object1 = gd_2d_plain(Three_Hump_Camel,Three_Hump_Camel_gradient)
+res1 = object1.minimize(x0,0.001,tol=1e-8)
+print('function value of plain method : ',np.around(res1.fval,5))
+
+# Momentum gd
+object2 = gd_2d_momentum(Three_Hump_Camel,Three_Hump_Camel_gradient)
+res2 = object2.minimize(x0,0.001,tol=1e-8)
+print('function value of momentum method : ',np.around(res2.fval,5))
+
+# NAG
+object3 = gd_2d_NAG(Three_Hump_Camel,Three_Hump_Camel_gradient)
+res3 = object3.minimize(x0,0.001,tol=1e-8)
+print('function value of NAG method : ',np.around(res3.fval,5))
+
