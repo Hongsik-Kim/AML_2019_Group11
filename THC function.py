fe88979f653a99d4ddf76a6ab6943b68a1180aef
Hongsik-Kim <52567223+Hongsik-Kim@users.noreply.github.com>
2019-07-09 19:17:19 +0100

    Update THC function.py

--- THC function.py
+++ THC function.py
@@ -9,9 +9,7 @@ Group 11
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 import os
-import time
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
@@ -32,6 +30,7 @@ def Three_Hump_Camel_gradient(x):
 #=========================================================================================================================================================================================================================
 
 
+# Plotting the THC function
 x1 = np.linspace(-2,2,100)
 x2 = np.linspace(-2,2,100)
 x1grid, x2grid = np.meshgrid(x1, x2)
@@ -49,18 +48,27 @@ plt.show()
 
 #=========================================================================================================================================================================================================================
 
+# Importing 2-dimensional gradient descent module
 from gd_2d import gd_2d_plain, gd_2d_momentum, gd_2d_NAG
 
+# Initial value
 x0 = [2,2]
+
+#=========================================================================================================================================================================================================================
+
 # Plain vanilla gd
 object1 = gd_2d_plain(Three_Hump_Camel,Three_Hump_Camel_gradient)
 res1 = object1.minimize(x0,0.001,tol=1e-8)
 print('function value of plain method : ',np.around(res1.fval,5))
 
+#=========================================================================================================================================================================================================================
+
 # Momentum gd
 object2 = gd_2d_momentum(Three_Hump_Camel,Three_Hump_Camel_gradient)
 res2 = object2.minimize(x0,0.001,tol=1e-8)
 print('function value of momentum method : ',np.around(res2.fval,5))
+
+#=========================================================================================================================================================================================================================
 
 # NAG
 object3 = gd_2d_NAG(Three_Hump_Camel,Three_Hump_Camel_gradient)
